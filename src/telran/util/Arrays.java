afb039dfb7e556c64e93f1384005239a6215fed6
Anastasiia <nastenabeloded@gmail.com>
2023-06-10 23:31:55 +0300

    cw-5 part 1

---
+++ src/telran/util/Arrays.java
@@ -0,0 +1,44 @@
+package telran.util;
+import static java.util.Arrays.*;
+
+public class Arrays {
+	public static <T> T[] addObject(T[] array, T obj) {
+		T[] res = copyOf(array, array.length + 1);
+		res[array.length] = obj;
+		return res;
+	}
+	
+	public static <T> T[] insertObject(T[] array, int index, T obj) {
+		T[] res = copyOf(array, array.length + 1);
+		System.arraycopy(array, 0, res, 0, index);
+		System.arraycopy(array, index, res, index + 1, array.length - index);
+		res[index] = obj;
+		
+		return res;
+	}
+	
+	public static <T> void bubbleSort(T[] array) {
+		boolean flSorted = false;
+		int length = array.length;
+		
+		do {
+			flSorted = true;
+			length--;
+			
+			for (int i = 0; i < length; i++) {
+				
+			}
+			
+		} while (!flSorted);
+	}
+	
+	private static <T> void swap(T[] array, int i, int j) {
+		T tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+	
+	
+	
+
+}
---
+++ src/telran/util/tests/ArraysTest.java
@@ -0,0 +1,34 @@
+package telran.util.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static telran.util.Arrays.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArraysTest {
+	Integer[] numbers = {10, 5, -5, 100, 200};
+	String[] strings = {"lmn", "a", "ab", "abc", "b", "ba"};
+	
+	
+	@Test
+	void addObjectTest() {
+		Integer[] expectedNumbers = {10, 5, -5, 100, 200, 50};
+		assertArrayEquals(expectedNumbers, addObject(numbers, 50));
+		
+		String[] expectedStrings = {"lmn", "a", "ab", "abc", "b", "ba", "bb"};
+		assertArrayEquals(expectedStrings, addObject(strings, "bb"));
+	}
+	
+	@Test
+	void insertObjectTest() {
+		Integer[] expectedLast = {10, 5, -5, 100, 200, 50};
+		Integer[] expectedFirst = {50, 10, 5, -5, 100, 200};
+		Integer[] expectedMiddle = {10, 5, 50, -5, 100, 200};
+		
+		assertArrayEquals(expectedLast, insertObject(numbers, 5, 50));
+		assertArrayEquals(expectedFirst, insertObject(numbers, 0, 50));
+		assertArrayEquals(expectedMiddle, insertObject(numbers, 2, 50));
+	}
+
+}
