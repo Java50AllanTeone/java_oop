7e55ecae56953d9bc7eb081b46e4524e85775979
allanteone <allanteone@gmail.com>
2023-06-21 00:49:24 +0300

    HW-8 solution

--- src/telran/exceptions/test/ExceptionsTest.java
+++ src/telran/exceptions/test/ExceptionsTest.java
@@ -9,6 +9,29 @@ import telran.exceptions.BallBrokenFloor;
 class ExceptionsTest {
 
 	
+	@Test
+	void ballBrokenFloorTest() {
+		for (int i = 0; i < 100_000; i++) {
+			var bbf = new BallBrokenFloor(200);
+			assertEquals(bbf.getFloor(), getMinFloor(bbf));
+		}
+	}
 
+	private int getMinFloor(BallBrokenFloor bbf) {
+		int left = 0;
+		int right = bbf.getnFloors();
+		int middle = (left + right) / 2;
+		
+		while (left < right) {
+			try {
+				bbf.broken(middle);
+				left = middle + 1;
+			} catch (Exception e) {
+				right = middle;
+			}
+			middle = (left + right) / 2;
+		}
+		return middle;
+	}
 
 }
