b5dc2ed3476e5cf2de9109774ad464b8122b8ee2
allanteone <allanteone@gmail.com>
2023-06-21 00:48:47 +0300

    CW-8

---
+++ src/telran/exceptions/test/ExceptionsTest.java
@@ -0,0 +1,14 @@
+package telran.exceptions.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.exceptions.BallBrokenFloor;
+
+class ExceptionsTest {
+
+	
+
+
+}
