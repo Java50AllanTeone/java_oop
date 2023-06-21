9dd525e7974e451f3221f52414a03d5a44b477e4
allanteone <allanteone@gmail.com>
2023-06-21 22:53:07 +0300

    Final removing fixes

--- src/telran/people/Company.java
+++ src/telran/people/Company.java
@@ -39,14 +39,14 @@ public class Company {
 	public Employee[] getEmployeesByAge(int ageFrom, int ageTo) {
 		int curYear = Year.now().getValue();
 
-		Employee from = new Employee(0, 0, curYear - ageTo);
-		Employee to = new Employee(0, 0, curYear - ageFrom);
+		Employee from = new Employee(-1, 0, curYear - ageTo);
+		Employee to = new Employee(-1, 0, curYear - ageFrom);
 		return getFilteredArr(employeesAge, from, to, cmpAge);
 	}
 	
 	public Employee[] getEmployeesBySalary(int salaryFrom, int salaryTo) {
-		Employee from = new Employee(0, salaryFrom);
-		Employee to = new Employee(0, salaryTo, 0);
+		Employee from = new Employee(-1, salaryFrom);
+		Employee to = new Employee(-1, salaryTo);
 		
 		return getFilteredArr(employeesSalary, from, to, cmpSlr);
 	}
@@ -71,7 +71,6 @@ public class Company {
 	
 	public Employee getEmployee(int id) {
 		var res = getEmployeesById(id, id);
-		
 		return res.length == 0 ? null : res[0];
 	}
 	
@@ -114,7 +113,27 @@ public class Company {
 		employeesDepartment = removeFromArr(employeesDepartment, predicate);
 	
 		return prevSize > employeesId.length;
-		//TODO fix complexity after Daniel's answer
+	}
+	
+	public boolean removeEmployees(int id) {
+		try {
+			var employee = getEmployee(id);
+			
+			var indId = Arrays.binarySearch(employeesId, employee, cmpId);
+			var indSalary = Arrays.binarySearch(employeesSalary, employee, cmpSlr.thenComparing(cmpId));
+			var indAge = Arrays.binarySearch(employeesAge, employee, cmpAge.thenComparing(cmpId));
+			var indName = Arrays.binarySearch(employeesName, employee, cmpName.thenComparing(cmpId));
+			var indDepartment = Arrays.binarySearch(employeesDepartment, employee, cmpDep.thenComparing(cmpId));
+			
+			employeesId = removeFromArr(employeesId, indId);
+			employeesSalary = removeFromArr(employeesSalary, indSalary);
+			employeesAge = removeFromArr(employeesAge, indAge);
+			employeesName = removeFromArr(employeesName, indName);
+			employeesDepartment = removeFromArr(employeesDepartment, indDepartment);
+		} catch (NullPointerException e) {
+			return false;
+		}	
+		return true;
 	}
 	
 	
@@ -140,12 +159,19 @@ public class Company {
 		return Arrays.copyOf(employeesAge, index);
 	}
 	
+	private Employee[] removeFromArr(Employee[] src, int i) {
+		Employee[] res = new Employee[src.length - 1];
+		System.arraycopy(src, 0, res, 0, i);
+		System.arraycopy(src, i + 1, res, i, res.length - i);
+		return res;
+	}
+	
 	private Employee[] getFilteredArr(Employee[] src, Employee from, Employee to, Comparator<Employee> comp) {
 		int start = Arrays.binarySearch(src, from, comp.thenComparing(cmpId));
 		int end = Arrays.binarySearch(src, to, comp.thenComparing(Comparator.reverseOrder()));
 		
 		try {
-			return Arrays.copyOfRange(src, getValidIndex(start), getValidIndex(end));
+			return Arrays.copyOfRange(src, getValidIndex(start), end >= 0 ? ++end : getValidIndex(end));
 		} catch (Exception e) {
 			return new Employee[] {};
 		}
